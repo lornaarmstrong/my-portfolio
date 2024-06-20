b6cb5003a3e0b15c3920a7f2fbac2aa12a522a53
lornaarmstrong <lornaarmstrong0304@gmail.com>
2024-06-20 16:50:20 +0100

    Introduction to List Group

---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+function Message() {
+    const name = `Lorna Armstrong`;
+    return <h1>{name}</h1>;
+}
+
+// We need to export the function so that we can import it in other files
+export default Message;
